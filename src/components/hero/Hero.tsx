36504c531e82828bd8d78f83af151cc47e23e319
m.pakravan <mahdipakravan79@gmail.com>
2025-08-24 15:36:48 +0330

    finalize hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -23,9 +23,24 @@ const HeroSection = () => {
               <h1>بانکی که</h1>
               <h1>آرزو</h1>
               <h1>داشتید</h1>
-              <Button className={s["hero-btn"]}>
-                اضافه شدن به ۵.۳ میلیون کاربر
-              </Button>
+              <a href="#">
+                به جمع ۵.۴ میلیون کاربر بپیوندید
+                <div className={s["arrow-wrap"]}>
+                  <div className="icon">
+                    <svg
+                      viewBox="0 0 24 24"
+                      width="100%"
+                      height="100%"
+                      aria-hidden="true"
+                      role="img"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M5.7,1.1c1.1-1.1,2.9-1.1,4,0l8.9,8.9c1.1,1.1,1.1,2.9,0,4l-8.9,8.9c-1.1,1.1-2.9,1.1-4,0c-1.1-1.1-1.1-2.9,0-4 l6.9-6.9L5.7,5.1C4.6,4,4.6,2.2,5.7,1.1L5.7,1.1z"></path>
+                    </svg>
+                  </div>
+                </div>
+              </a>
             </div>
 
             <div className={s["hero-cards"]}>
